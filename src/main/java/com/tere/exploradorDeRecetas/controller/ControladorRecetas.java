99725e82088b6759b4cd59e6f0cc513becfa2007
Tere <teresaolivaresrueda@gmail.com>
2026-06-27 20:11:06 -0400

    carpeta web-inf- pom dependencias- properties listas

---
+++ src/main/java/com/tere/exploradorDeRecetas/controller/ControladorRecetas.java
@@ -0,0 +1,8 @@
+package com.tere.exploradorDeRecetas.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ControladorRecetas {
+
+}
---
+++ src/test/java/com/tere/exploradorDeRecetas/ExploradorDeRecetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tere.exploradorDeRecetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploradorDeRecetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
